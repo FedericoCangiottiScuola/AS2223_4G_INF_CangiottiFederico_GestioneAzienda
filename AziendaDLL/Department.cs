5e301ee7e8bbb47d7378dcbc74aab269becb0385
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-12-13 15:30:04 +0100

    feat: Implementa la classe Department

--- AziendaDLL/Department.cs
+++ AziendaDLL/Department.cs
@@ -2,5 +2,50 @@
 {
     public class Department
     {
+        string name;
+        Employee departmentHead;
+        int nMaxEmployees;
+        int nEmployees;
+        List<Employee> employees;
+
+        public Department(string name, Employee departmentHead, int nMaxEmployees)
+        {
+            this.name = name;
+            this.departmentHead = departmentHead;
+            this.nMaxEmployees = nMaxEmployees;
+            nEmployees = 0;
+            employees = new List<Employee>();
+        }
+
+        public bool AddEmployee(Employee employee)
+        {
+            if (nEmployees < nMaxEmployees)
+            {
+                employees.Add(employee);
+                nEmployees++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string GetEmployees()
+        {
+            string ris = "";
+            int i = 1;
+            foreach(Employee emp in employees)
+            {
+                ris += $"{i}) {emp}";
+                i++;
+            }
+            return ris;
+        }
+
+        public void ExpandDepartment(int newLength)
+        {
+            nMaxEmployees = newLength;
+        }
     }
 }
