26fa313d0c1b6f7b3d0a60f7a85dfddff3da1f50
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-12-13 15:35:00 +0100

    feat: Implementa la classe UIDesigner

--- AziendaDLL/UIDesigner.cs
+++ AziendaDLL/UIDesigner.cs
@@ -2,5 +2,11 @@
 {
     public class UIDesigner
     {
+        List<string> designPrograms;
+
+        public UIDesigner(List<string> designPrograms)
+        {
+            this.designPrograms = designPrograms;
+        }
     }
 }
