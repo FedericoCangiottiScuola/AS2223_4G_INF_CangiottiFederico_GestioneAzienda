98b5d6d57b0ee308565f8854a2f1c61f2e85d896
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-12-14 18:46:35 +0100

    refactor: Modifica codice entry point program.cs

--- AS2223_4G_INF_CangiottiFederico_GestioneAzienda/Program.cs
+++ AS2223_4G_INF_CangiottiFederico_GestioneAzienda/Program.cs
@@ -8,18 +8,20 @@ namespace AS2223_4G_INF_CangiottiFederico_GestioneAzienda
         {
             Developer developer = new Developer("Elia", "Rossi", 24, (float)1335.23, new List<string> { "javascript","C#" });
             UIDesigner uidesigner = new UIDesigner("Ugo", "Bianchi", 31, (float)1420.69, new List<string> { "Adobe Photoshop","Adobe Illustrator","Figma" });
-            Administrator admin = new Administrator("Alberto", "Migani", 47, (float)2500.54, 8);
-            Department dipartimento = new Department("Prova", admin, 3);
+            Administrator administrator = new Administrator("Alberto", "Migani", 47, (float)2500.54, 8);
+            Department reparto = new Department("Grafica", administrator, 3);
 
-            Console.WriteLine(dipartimento.PrintInformations());    // stampa i dati del reparto
+            Console.WriteLine(reparto.PrintInformations());    // stampa i dati del reparto
 
-            Console.WriteLine(dipartimento.ExpandDepartment(5));   // incrementa il numero massimo di dipendenti se la nuova lunghezza è maggiore
+            Console.WriteLine(reparto.ExpandDepartment(5));   // incrementa il numero massimo di dipendenti se la nuova lunghezza è maggiore
 
-            dipartimento.AddEmployee(uidesigner);
+            reparto.AddEmployee(developer);
+            reparto.AddEmployee(uidesigner);
 
-            Console.WriteLine(dipartimento.PrintInformations());    // stampa i dati del reparto
+            Console.WriteLine(reparto.PrintInformations());    // stampa i dati del reparto
 
-            List<Employee> backup = dipartimento.GetEmployees();    // restituisce la lista dei dipendenti del reparto
+            List<Employee> backup = reparto.GetEmployees();    // restituisce la lista dei dipendenti del reparto
+            Console.WriteLine(backup[0]);
         }
     }
 }
